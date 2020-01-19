d1539f806148f2b9fd20b14895abb666ef39576a
Gerry <git@caulfield.me>
2020-01-19 15:48:58 +0100

    Improve the Linux build script
    It now includes the readme, license and examples as well as compressing
    the release.

--- build/build_anywhere.sh
+++ build/build_anywhere.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 GID=$(id -g $USER)
 docker build . --target builder -t compile_gencert && \
-mkdir -p $PWD/builds && \
-docker run --user $UID:$GID --rm -v $PWD/builds:/app compile_gencert cp /bin/gencert /app && \
-sudo chown $USER:$GID $PWD/builds/gencert
+mkdir -p $PWD/gencert-linux && \
+docker run --user $UID:$GID --rm -v $PWD/gencert-linux:/app compile_gencert cp /bin/gencert /app && \
+sudo chown $USER:$GID $PWD/gencert-linux/gencert
+cp -r examples LICENSE README.md gencert-linux
+mkdir -p releases
+tar -czvf releases/gencert-linux.tar.gz gencert-linux
